9e040605770fd55744449cd82d6ecdde4dea2a56
Eric Van Albert <eric@van.al>
2019-06-16 19:33:15 -0400

    Add two data methods, printf and tail. tail is better for larger programs

--- wrinkler.py
+++ wrinkler.py
@@ -5,21 +5,23 @@ import sys
 import shlex
 import argparse
 
-compressor_modes = ["null", "lzma", "xz", "gzip", "bzip2"]
+compressor_modes = ["lzma", "xz", "gzip", "bzip2"]
+data_modes = ["printf", "tail"]
 
 interpreters = {
     "sh": "|sh",
     "python": "|python",
     "python2": "|python2",
     "python3": "|python3",
-    "bin": ">a;chmod +x a;./a",
+    "bin": ">a;/lib/ld*o ./a",   # smol
+    "bin2": ">a;chmod +x a;./a", # 2 bytes larger but probably more robust
 }
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help='Input file to compress')
     parser.add_argument('-o', '--output', help='Output file to compress')
-    parser.add_argument('-p', '--interpreter', help='Interpreter to use ({})'.format(", ".join(interpreters)))
+    parser.add_argument('-p', '--interpreter', default="sh", help='Interpreter to use ({})'.format(", ".join(interpreters)))
     args = parser.parse_args()
 
     if args.input is None:
@@ -47,42 +49,43 @@ def replace_with_octal(cd, char):
     return cd
 
 def best_compression(input_binary, *args, **kwargs):
-    results = [compress(input_binary, mode, *args, **kwargs)
-        for mode in compressor_modes]
-    for mode, result in zip(compressor_modes, results):
+    modes = [("null", "null")] + [(zip_mode, data_mode) for zip_mode in compressor_modes for data_mode in data_modes]
+    results = [compress(input_binary, *mode, *args, **kwargs)
+        for mode in modes]
+    for mode, result in zip(modes, results):
         print("Method", mode, "gives size", len(result))
-    best = min(zip(compressor_modes, results), key=lambda x: len(x[1]))
+    best = min(zip(modes, results), key=lambda x: len(x[1]))
     print(best[0], "is the winner, with size", len(best[1]))
     return best[1]
 
-def compress(input_binary, method="lzma9", interpreter="|sh"):
+def compress(input_binary, zip_method="lzma9", data_method="printf", interpreter="|sh"):
     # Map from common name to (compressor, decompressor) tuple
-    if method == "null":
+    if zip_method == "null":
         return input_binary
 
-    methods = {
+    zip_methods = {
         "lzma": ("xz -F lzma -cze9", "|unxz"),
         "xz": ("xz -F xz -cze9", "|unxz"),
         "gzip": ("gzip -9", "|zcat"),
         "bzip2": ("bzip2 -9", "|bzcat"),
     }
-    (compressor, decompressor) = methods[method]
+    (compressor, decompressor) = zip_methods[zip_method]
 
     proc = subprocess.Popen(compressor, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     cd = proc.communicate(input_binary)[0]
     if proc.returncode != 0:
         raise ValueError("Process exited with returncode {}".format(proc.returncode))
 
-    #lencd = len(cd)
-    #print("Length of compressed data is", lencd)
-    cd = cd.replace(b'\\', b'\\\\')
-    cd = cd.replace(b'%', b'%%')
-    cd = replace_with_octal(cd, b'\0')
-    cd = replace_with_octal(cd, b"'")
-    #lensecd = len(cd)
-    #print("Length shell-escaped compressed data is", lensecd, "(ratio", lensecd / lencd, ")")
-
-    return b"printf '" + cd + b"'" + bytes(decompressor, encoding='latin1') + bytes(interpreter, encoding='latin1')
+    if data_method == "printf":
+        cd = cd.replace(b'\\', b'\\\\')
+        cd = cd.replace(b'%', b'%%')
+        cd = replace_with_octal(cd, b'\0')
+        cd = replace_with_octal(cd, b"'")
+        return b"printf '" + cd + b"'" + bytes(decompressor, encoding='latin1') + bytes(interpreter, encoding='latin1')
+    elif data_method == "tail":
+        return b"tail -n+2 $0" + bytes(decompressor, encoding='latin1') + bytes(interpreter, encoding='latin1') + b";exit\n" + cd
+    else:
+        raise ValueError("Unknown data method")
 
 if __name__ == "__main__":
     main()
